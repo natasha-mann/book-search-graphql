3e0264069e343c9915bdcda6514b3f0ce2fe5b4a
Natasha Mann <75756325+natasha-mann@users.noreply.github.com>
2021-07-30 23:22:01 +0100

    updated saveBook resolver

--- server/resolvers/saveBook.js
+++ server/resolvers/saveBook.js
@@ -1,5 +1,24 @@
-const saveBook = () => {
-  return;
+const saveBook = (_, { input }, context) => {
+  try {
+    if (context.user) {
+      const { bookId, authors, title, description, image } = input;
+      const updatedUser = await User.findOneAndUpdate(
+        { _id: context.user._id },
+        {
+          $addToSet: {
+            savedBooks: { bookId, authors, title, description, image },
+          },
+        },
+        { new: true, runValidators: true }
+      );
+      return updatedUser;
+    } else {
+      throw new AuthenticationError("Not authorised");
+    }
+  } catch (err) {
+    console.log(err);
+    throw new AuthenticationError("Something went wrong!");
+  }
 };
 
 module.exports = saveBook;
