df44920b5d3773d03485872bef358bd3814d6eed
Natasha Mann <75756325+natasha-mann@users.noreply.github.com>
2021-07-30 23:26:44 +0100

    added remove book resolver

--- server/resolvers/removeBook.js
+++ server/resolvers/removeBook.js
@@ -1,5 +1,23 @@
-const removeBook = () => {
-  return;
+const { User } = require("../models");
+
+const removeBook = (_, { input }, context) => {
+  if (context.user) {
+    const { bookId } = input;
+
+    const updatedUser = await User.findOneAndUpdate(
+      { _id: context.user._id },
+      { $pull: { savedBooks: { bookId } } },
+      { new: true }
+    );
+
+    if (!updatedUser) {
+      throw new Error("Couldn't find user with this id!");
+    }
+
+    return updatedUser;
+  } else {
+    throw new AuthenticationError("Not authorised");
+  }
 };
 
 module.exports = removeBook;
