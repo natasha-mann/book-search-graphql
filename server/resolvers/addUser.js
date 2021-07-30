1754313cc358fb75065c50471125224ad5235887
Natasha Mann <75756325+natasha-mann@users.noreply.github.com>
2021-07-30 23:17:09 +0100

    updated addUser resolver

--- server/resolvers/addUser.js
+++ server/resolvers/addUser.js
@@ -1,5 +1,17 @@
-const addUser = () => {
-  return;
+const { User } = require("../models");
+const { signToken } = require("../utils/auth");
+
+const addUser = async (_, { input }) => {
+  const { username, email, password } = input;
+  const user = await User.create({ username, email, password });
+
+  if (!user) {
+    throw new AuthenticationError("Something went wrong!");
+  }
+
+  const token = signToken(user);
+
+  return { token, user };
 };
 
 module.exports = addUser;
