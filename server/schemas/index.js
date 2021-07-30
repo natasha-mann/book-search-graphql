e0cb2c1da233221b65505dac37667959fe13fab3
Natasha Mann <75756325+natasha-mann@users.noreply.github.com>
2021-07-30 22:39:05 +0100

    set up graphql schema

---
+++ server/schemas/index.js
@@ -0,0 +1,57 @@
+const { gql } = require("apollo-server-express");
+
+const typeDefs = gql`
+  type Book {
+    bookId: String!
+    authors: [author!]
+    description: String!
+    title: String!
+    image: String!
+    link: String!
+  }
+
+  type User {
+    _id: ID!
+    username: String!
+    email: String!
+    bookCount: Int!
+    savedBooks: [Book]
+  }
+
+  type Auth {
+    token: ID!
+    user: User!
+  }
+
+  type Query {
+    me: User
+  }
+
+  input LoginInput {
+    email: String!
+    password: String!
+  }
+
+  input AddUserInput {
+    username: String!
+    email: String!
+    password: String!
+  }
+
+  input SaveBookInput {
+    bookId: String!
+    authors: [String!]
+    title: String!
+    description: String!
+    image: String!
+  }
+
+  type Mutation {
+    login(input: LoginInput): Auth!
+    addUser(input: AddUserInput): Auth!
+    saveBook(input: SaveBookInput): User!
+    removeBook(bookId: String!): User!
+  }
+`;
+
+module.exports = typeDefs;
