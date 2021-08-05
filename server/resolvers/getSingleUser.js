4611d6b1019e1b9e83b9c9b81c7b9316a798f173
Natasha Mann <75756325+natasha-mann@users.noreply.github.com>
2021-08-05 16:34:57 +0100

    fixed get user resolver

--- server/resolvers/getSingleUser.js
+++ server/resolvers/getSingleUser.js
@@ -1,13 +1,22 @@
 const { User } = require("../models");
 
-const getSingleUser = async (_, args) => {
-  const foundUser = await User.findOne({});
+const getSingleUser = async (_, args, context) => {
+  try {
+    if (context.user) {
+      const foundUser = await User.findById({ _id: context.user.id });
 
-  if (!foundUser) {
-    throw new Error("Couldn't find user with this id!");
+      if (!foundUser) {
+        throw new Error("Couldn't find user with this id!");
+      }
+
+      return foundUser;
+    } else {
+      throw new AuthenticationError("Not authorised");
+    }
+  } catch (err) {
+    console.log(err);
+    throw new AuthenticationError("Something went wrong!");
   }
-
-  return foundUser;
 };
 
 module.exports = getSingleUser;
