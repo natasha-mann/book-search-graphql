fd1d2a75c99068e8b4b241c766fc911e74f9e52c
Natasha Mann <75756325+natasha-mann@users.noreply.github.com>
2021-08-09 18:21:45 +0100

    updated graphql link

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,7 @@ import SavedBooks from "./pages/SavedBooks";
 import Navbar from "./components/Navbar";
 
 const httpLink = createHttpLink({
-  uri: process.env.GRAPHQL_URL || "http://localhost:3001/graphql",
+  uri: "/graphql",
 });
 
 const authLink = setContext((_, { headers }) => {
